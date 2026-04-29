d71886f72c77d4a91089eef890410f1ad1fcc6e2
CristoferMantilla <cristofermantilla1@gmail.com>
2026-04-29 17:38:22 -0500

    primer commit

---
+++ src/progobjetos_t1/ProgObjetos_T1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progobjetos_t1;
+
+/**
+ *
+ * @author crist
+ */
+public class ProgObjetos_T1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
