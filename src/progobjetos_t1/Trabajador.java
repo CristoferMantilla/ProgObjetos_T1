4d985b795be5e6c1e5e3564421de30ef97dc76a0
CristoferMantilla <cristofermantilla1@gmail.com>
2026-04-29 20:29:12 -0500

    se agregaron get y set

--- src/progobjetos_t1/Trabajador.java
+++ src/progobjetos_t1/Trabajador.java
@@ -10,6 +10,99 @@ package progobjetos_t1;
  */
 public class Trabajador 
 {
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) 
+    {
+        if(tipoDocumento.equalsIgnoreCase("dni") || tipoDocumento.equalsIgnoreCase("ce"))
+        {
+            this.tipoDocumento=tipoDocumento;
+            System.out.println("valor correcto");
+        }else{
+            System.out.println("Error tipo docuemnto inválido");
+        }
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) 
+    {
+        if(this.tipoDocumento.equalsIgnoreCase("dni")){
+            if(numeroDocumento.length()==8){
+                this.numeroDocumento = numeroDocumento;
+            }
+            else{
+                System.out.println("Error cantidad de digitos debe ser 8");
+            }
+        }else if(this.tipoDocumento.equalsIgnoreCase("ce")){
+            if(numeroDocumento.length()==9){
+                this.numeroDocumento = numeroDocumento;
+            }else{
+                System.out.println("Error cantidad de digitos debe ser 9");
+            }
+        }
+    }
+
+    public String getRegimenLaboral() {
+        return regimenLaboral;
+    }
+
+    public void setRegimenLaboral(String regimenLaboral) {
+        this.regimenLaboral = regimenLaboral;
+    }
+
+    public String getFondoPension() {
+        return fondoPension;
+    }
+
+    public void setFondoPension(String fondoPension) {
+        this.fondoPension = fondoPension;
+    }
+
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public int getCantidadHijos() {
+        return cantidadHijos;
+    }
+
+    public void setCantidadHijos(int cantidadHijos) {
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
     private String codigo;
     private String nombre;
     private String tipoDocumento;
@@ -20,14 +113,6 @@ public class Trabajador
     private int cantidadHijos;
     private String turno;
     
-    private void validarDocumento(String tipo, String numero) {
-        if (tipo.equalsIgnoreCase("DNI") && numero.length() != 8) {
-            System.out.println("El DNI debe tener 8 dígitos.");
-        }
-        if (tipo.equalsIgnoreCase("Carnet de Extranjeria") && numero.length() != 9) {
-            System.out.println("El Carnet de Extranjeria debe tener 9 dígitos.");
-        }
-    }
     public void VerDatos(){
         System.out.println("Cod: "+this.codigo+" Nombre: "+this.nombre+" TipoDoc: "
                 +this.tipoDocumento+" NroDoc:"+this.numeroDocumento+
